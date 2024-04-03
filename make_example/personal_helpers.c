6250996dbbb15afa21a3d4fcfa9a8341a35f85b8
PhilippTheSurfer <philipp.lehmann@gruppe.ai>
2024-04-03 21:41:47 +0200

    add example for make

---
+++ make_example/personal_helpers.c
@@ -0,0 +1,7 @@
+// personal_helpers.c
+#include "personal_helpers.h"
+#include <stdio.h>
+
+void print_hello() {
+    printf("Hello from personal_helpers!\n");
+}
---
+++ make_example/personal_helpers.h
@@ -0,0 +1,7 @@
+// personal_helpers.h
+#ifndef PERSONAL_HELPERS_H
+#define PERSONAL_HELPERS_H
+
+void print_hello();
+
+#endif // PERSONAL_HELPERS_H
